683c41b7be637f413044916ffc96d502f69b2d90
yachiyachi549 <yachiyachi549@gmail.com>
2026-05-20 14:24:22 +0900

    Revert "[add]score"
    This reverts commit d058604aa2ba5c41c910702641a12ec61bd1adee.

--- Assets/tsuchiya/Score.cs
+++ Assets/tsuchiya/Score.cs
@@ -10,7 +10,7 @@ public class Score : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        text.text = "aga";  
+        text.text = "abc";  
     }
 
     // Update is called once per frame
