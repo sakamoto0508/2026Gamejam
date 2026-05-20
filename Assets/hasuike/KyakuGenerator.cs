b81ca69bc1c1a5b13f1228139cded98d7965b1bc
Hasuike K41to <k4it0.h45u1k3@gmail.com>
2026-05-20 12:27:59 +0900

    [add]CustomerGenerator

---
+++ Assets/hasuike/KyakuGenerator.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KyakuGenerator : MonoBehaviour
+{
+    public List<GameObject> Customers = new List<GameObject>();
+
+    public void CustomerGenerator(int number)
+    {
+        Instantiate(Customers[number]);
+    }
+    private void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            int rand = Random.Range(0, Customers.Count);
+            CustomerGenerator(rand);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
