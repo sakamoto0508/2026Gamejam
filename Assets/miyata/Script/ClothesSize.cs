ee6aef8e6b822e293c78cdbdf8ea223d16fcc5e5
宮田瞬 <shungongtian501@gmail.com>
2026-05-20 14:12:39 +0900

    服にサイズの要素を付与しました

--- Assets/miyata/Script/ClothesSize.cs
+++ Assets/miyata/Script/ClothesSize.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class ClothesSize : MonoBehaviour
 {
-   
+    public  Size CurrentSize => currentsize;
+    [SerializeField] private Size currentsize;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
        public enum Size
         {
