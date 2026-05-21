2b8761cc7ff75c0d04465cabedb2296b248a3bb5
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-05-21 12:25:13 +0900

    [update]二重防止

--- Assets/terasimaminori/SceneLoader.cs
+++ Assets/terasimaminori/SceneLoader.cs
@@ -2,26 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Scene : MonoBehaviour
 {
 
     [SerializeField] private string _loadScene;
     [SerializeField] private float _loadTime = 0.2f;
+    [SerializeField] private Button _button;
 
     public void SceneChange()
     {
         _loadScene = "Game";
+        _button.interactable = false;
         StartCoroutine(LoadSceneCoroutine(_loadScene));
     }
     public void TitleChange()
     {
         _loadScene = "Title";
+        _button.interactable = false;
         StartCoroutine(LoadSceneCoroutine(_loadScene));
     }
     public void ResultChange()
     {
         _loadScene = "Result";
+        _button.interactable = false;
         StartCoroutine(LoadSceneCoroutine(_loadScene));
     }
 
